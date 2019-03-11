7f963eae15be4377952960b2a39467949f11c11c
Sourabh S. Rawat <dragneelfps@gmail.com>
2019-03-12 00:51:04 +0530

    add create page

--- products/views.py
+++ products/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'products/home.html')
+
+def create(request):
+    if request.method == 'POST':
+        print('req', request.POST);
+        return render(request, 'products/create.html')
+    else:
+        return render(request, 'products/create.html')
